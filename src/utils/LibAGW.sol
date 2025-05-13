15a094d11c60a5cee31065d825ec4b3e6cd295a9
Coffee☕️ <coffee@coffeebot.dev>
2025-05-13 17:01:35 -0400

    doc

--- src/utils/LibAGW.sol
+++ src/utils/LibAGW.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.0;
 
 import {IAGWRegistry} from "../interfaces/IAGWRegistry.sol";
 
+/// @notice Library for detecting and interacting with AGW contracts
+// @author Abstract (https://github.com/Abstract-Foundation/absmate/blob/main/src/utils/LibAGW.sol)
 library LibAGW {
     IAGWRegistry constant AGW_REGISTRY = IAGWRegistry(0xd5E3efDA6bB5aB545cc2358796E96D9033496Dda);
 
+    /// @dev returns true if the address is a deployed AGW contract
     function isAGWContract(address _address) internal view returns (bool) {
         return AGW_REGISTRY.isAGW(_address);
     }
--- src/utils/LibEVM.sol
+++ src/utils/LibEVM.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.0;
 import {ACCOUNT_CODE_STORAGE_SYSTEM_CONTRACT} from "era-contracts/system-contracts/Constants.sol";
 import {Utils} from "era-contracts/system-contracts/libraries/Utils.sol";
 
+/// @notice Library for detecting EVM comaptibility of addresses
+// @author Abstract (https://github.com/Abstract-Foundation/absmate/blob/main/src/utils/LibEVM.sol)
 library LibEVM {
+    
+    /// @dev returns true if the address is EVM compatible. Empty addresses are assumed to be EOAs
+    /// but could potentially be undeployed zkvm contracts so this should be used with caution.
     function isEVMCompatibleAddress(address _address) internal view returns (bool) {
         bytes32 codeHash = ACCOUNT_CODE_STORAGE_SYSTEM_CONTRACT.getRawCodeHash(_address);
         if (codeHash == 0x00) {
